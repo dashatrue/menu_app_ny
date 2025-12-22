bf542890435e120eb21cb1e1b1b3ba81cf7a722f
Dora <dashatrue34@gmail.com>
2025-12-22 23:24:07 +0500

    add test endpoint

--- menu/urls.py
+++ menu/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse  # Добавьте этот импорт
+
+# Простейший view для теста
+def test_view(request):
+    return HttpResponse("Django работает!")
 
 urlpatterns = [
+    path('test/', test_view),
     path('admin/', admin.site.urls),
     path('', include('menu_app.urls')),
 ]
