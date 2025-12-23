8a2e593706ce0c8b2b28cc430280cb0039d1c898
Dora <dashatrue34@gmail.com>
2025-12-23 21:09:35 +0500

    fixing

--- menu_app/forms.py
+++ menu_app/forms.py
@@ -28,6 +28,26 @@ class OrderForm(forms.Form):
         widget=forms.CheckboxSelectMultiple(attrs={'class': 'd-none'})
     )
 
+    customer_name = forms.CharField(
+        label='Ваше имя',
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Напиши свое имя :)'
+        })
+    )
+
+    comments = forms.CharField(
+        label="Комментарии к заказу",
+        required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 3,
+            'placeholder': 'Особые пожелания...'
+        })
+    )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
